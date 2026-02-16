35a4f5c79bdac46104365ebb766692c09d3da1a0
transformaciondigital-PIT <transformacion.digital@grupopit.com>
2026-02-16 10:34:05 -0600

    Update TopContextBar.tsx

--- src/components/TopContextBar.tsx
+++ src/components/TopContextBar.tsx
@@ -1,7 +1,6 @@
 import { useRef, useState } from "react";
 import { ALMACENES, CENTROS, MATERIALS, MONEDAS } from "../catalogs";
 import type { CsvImportMode } from "../csvImport";
-import { ALMACENES, CENTROS, MATERIALS, MONEDAS } from "../catalogs";
 import type { ContextState } from "../types";
 
 type Props = {
